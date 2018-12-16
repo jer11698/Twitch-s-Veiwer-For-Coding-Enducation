feb63998d75d81b6cce1c672de2d2d7b2766962c
jer11698 <jer11698.553@outlook.com>
2018-12-16 18:04:12 +0700

    add comment

--- csvtop10game.py
+++ csvtop10game.py
@@ -11,25 +11,31 @@ import pygal
 
 
 def main():
-    for i in [li1, li2, li3, li4, li5, li6, li7]:
-        dic = {}
-        name, date = i.list_name()
-        for i in name:
-            data = pd.read_csv(i, sep="\t", header=None, quotechar=None, quoting=3)
+    """ เริ่มจากตรงนี้ """
+    for i in [li1, li2, li3, li4, li5, li6, li7]:  # รายชื่อข้อมูลแต่ละวันมาทำที่ละไฟล์
+        dic = {}  # ไว้เก็บข้อมูลที่รวมไว้แล้ว
+        name, date = i.list_name()  # รายชื่อข้อมูลแต่ละเวลาของ li1-7
+        for i in name:  # ไฟลข้อมูลรายชื่อของแต่ละเวลาของ li1-7 ทำทีละไฟล์
+            data = pd.read_csv(i, sep="\t", header=None,
+                               quotechar=None, quoting=3)
             data.columns = ["s_id", "current_views", "created", "g_name", "b_id", "b_name", "delay", "follower",
                             "status", "b_language", "total_views", "language", "b_created", "playback", "resolution", "var"]
-            dic = cal(data, dic)
+            dic = cal(data, dic)  # เรียกใช้ฟังก์ชัน
         graph = chart(dic, date)
+        # ทำเป็นตารางแล้วเรนเดอร์เป็นไฟล์ csv
         data_filter = pd.DataFrame(
             {'game': pd.Categorical(dic), 'views': pd.Categorical(dic.values())})
-        # print(data_filter)
         data_filter.to_csv(date+'.csv')
+        # เรนเดอร์เป็นกราฟ
         graph.render_to_file(date+".svg")
 
 
 def cal(data, dic):
+    """ ส่วนรวมข้อมูล """
+    # เลือกดึงตารางข้อมูลออกมา
     view = data["current_views"].tolist()
     game = data["g_name"].tolist()
+    # ส่วนของการรวมยอดวิวแต่ละเกม
     for i in range(len(game)):
         if game[i] not in dic:
             dic[game[i]] = view[i]
@@ -39,9 +45,12 @@ def cal(data, dic):
 
 
 def chart(dic, date):
+    """ ส่วนทำกราฟ """
+    # สลับข้อมูลและคีย์
     dic2 = {}
     for i in dic:
         dic2[dic[i]] = i
+    # ได้เวลาทำกราฟ
     graph = pygal.Pie()
     graph.title = "Top 10 GAME view "+date
     count = 0
--- themost.py
+++ themost.py
@@ -5,12 +5,14 @@ import datetime as dt
 
 
 def main():
-    name = {}
+    name = {}  # ไว้เก็บข้อมูลที่แบ่งเป็นหมวดไว้แล้ว
     for i in ("2015-02-01.csv", "2015-02-02.csv", "2015-02-03.csv", "2015-02-04.csv", "2015-02-05.csv", "2015-02-06.csv", "2015-02-07.csv"):
         data = pd.read_csv("nope/"+i, sep=",")
         data.drop(['Unnamed: 0'], axis=1, inplace=True)
+        # เลือกดึงตารางข้อมูลออกมา
         game = data['game'].tolist()
         view = data['views'].tolist()
+        # คัดยอดวิวเกมมาจัดเป็นหมวดเดียวกัน
         for i in range(len(game)):
             if game[i] not in name:
                 name[game[i]] = [view[i]]
@@ -20,13 +22,15 @@ def main():
 
 
 def most(name):
+    # เรียงข้อมูลจากมากไปน้อย
     sorted_game = {}
     count = 0
     for i in name:
         sorted_game[i] = sum(name[i])
     sorted_game = sorted(name.items(), key=lambda kv: kv[1], reverse=True)
+    # ได้เวลาทำกราฟ
     line_chart = pygal.Line()
-    line_chart.title = 'The Most Games\' view in Twitch 2015-02-01 to 2015-02-07' 
+    line_chart.title = 'The Most Games\' view in Twitch 2015-02-01 to 2015-02-07'
     line_chart.x_labels = map(lambda d: d.strftime('%Y-%m-%d'), [
         dt.datetime(2015, 2, 1),
         dt.datetime(2015, 2, 2),
@@ -41,6 +45,7 @@ def most(name):
         if count == 14:
             break
         count += 1
+    # เรนเดอร์กราฟออกมา
     line_chart.render_to_file('chart.svg')
 
 
